2896effce75ac803f9bacf9ae6f3ecbf35c7fa5a
reid <reidsherman@gmail.com>
2018-01-04 17:29:15 -0800

    Structure quicksort in place

--- quicksort_inplace.rb
+++ quicksort_inplace.rb
@@ -1,5 +1,23 @@
 
 #return if length <= 1
-#select pivot
-#partition array 
-#
+#select pivot (first el)
+#partition array around pivot
+#recurse on left and right 
+#pathological case: we get an array that's already sorted. n level deep tree
+def quicksort(arr, s_idx = 0, e_idx = arr.length)
+  if s_idx == e_idx 
+
+  end 
+  pivot_idx = 0
+  barrier_idx = 0
+  i = 1 
+  while i < arr.length 
+    if arr[i] < arr[pivot_idx]
+      barrier_idx += 1
+    else 
+      #swap 
+    end 
+    i+=1
+  end 
+end 
+input = [4,3,10,7,2,8,1]
