0f25ff27b1a4cdb0c67f8ffc436380566eb09fd7
reid <reidsherman@gmail.com>
2018-01-09 14:07:07 -0800

    Start max contiguous subarray

--- max_subarray.js
+++ max_subarray.js
@@ -1,10 +1,18 @@
+//Doesn't work correctly 
 function maxSubArray(array) {
-  let max = 0;
-  let current_sum = 0;
-  for(let i = 0; i < array.length; i++) {
-    current_sum += array[i];
-    if (current_sum > max) {
-      max = current_sum;
-    } 
+  let maxEndingHere = array[0];
+  let currentMax = array[0];
+  let sum = array[0];
+  for(let i = 1; i < array.length; i++) {
+    sum += array[i];
+    if (maxEndingHere > currentMax) {
+      currentMax = maxEndingHere;
+    } else {
+      maxEndingHere = 0;  
+    }
   }
+  return currentMax;
 }
+
+let result = maxSubArray([3,-1,4,-1,5,-10,4]);
+console.log(result);
